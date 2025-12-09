fc3a8d95c574ec67e047944948af203324d1a810
sanjai2908 <sanjais2908@gmail.com>
2025-12-09 11:46:58 +0530

    Modify axios baseURL for local development
    Updated baseURL in axios instance for local development.

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+import axios from "axios";
+
 const api = axios.create({
-  baseURL:process.env.REACT_APP_API_URL || "http://localhost:5000/api", // fallback for local dev
+  baseURL:
+    process.env.REACT_APP_API_URL || "http://localhost:5000", // local fallback
 });
+
 
 api.interceptors.request.use((config) => {
   const auth = localStorage.getItem("auth");
