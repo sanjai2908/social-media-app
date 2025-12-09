53b7afd7e56a00d0724b96ff46f85f599b7e78d5
sanjai2908 <sanjais2908@gmail.com>
2025-12-09 16:00:28 +0530

    Integrate socket.io for chat message updates
    Added real-time chat updates using socket.io.

--- frontend/src/components/ChatList.jsx
+++ frontend/src/components/ChatList.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { ListGroup } from "react-bootstrap";
 import api from "../api/axiosInstance";
+import { io } from "socket.io-client";
+
+const socket = io("https://YOUR-BACKEND-RENDER-LINK");
 
 const ChatList = ({ onSelectChat }) => {
   const [chats, setChats] = useState([]);
@@ -14,15 +17,18 @@ const ChatList = ({ onSelectChat }) => {
     fetchChats();
   }, []);
 
+  useEffect(() => {
+    socket.on("receiveMessage", () => {
+      fetchChats();
+    });
+    return () => socket.off("receiveMessage");
+  }, []);
+
   return (
     <ListGroup>
       {chats.map((chat) => (
-        <ListGroup.Item
-          key={chat._id}
-          action
-          onClick={() => onSelectChat(chat)}
-        >
-          {chat.users?.map((u) => u.name).join(", ")}
+        <ListGroup.Item key={chat._id} action onClick={() => onSelectChat(chat)}>
+          {chat.users.map((u) => u.name).join(", ")}
         </ListGroup.Item>
       ))}
     </ListGroup>
