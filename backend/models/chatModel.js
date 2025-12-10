3bdb8f0698ae52ec22f2b9da51994be65ad99f60
sanjai2908 <sanjais2908@gmail.com>
2025-12-10 15:47:59 +0530

    Add 'seen' field to message schema

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema(
   {
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    content: { type: String, required: true }
+    content: { type: String, required: true },
+    seen: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
+
 
 const chatSchema = new mongoose.Schema(
   {
